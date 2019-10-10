7a014859773036e28b5da01bad2bfa1f6ec0244f
Natarajan Swaminathan <snatrajs@gmail.com>
2019-10-10 09:04:36 +0530

    Model Mapper

---
+++ src/main/java/com/stacksimplify/restservices/dtos/UserMmDto.java
@@ -0,0 +1,20 @@
+package com.stacksimplify.restservices.dtos;
+
+public class UserMmDto {
+	private Long Userid;
+	private String Username;
+	public Long getUserid() {
+		return Userid;
+	}
+	public void setUserid(Long userid) {
+		Userid = userid;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	
+	
+}
